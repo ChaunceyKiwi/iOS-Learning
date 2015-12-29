68c1e4ecc1927feeb5a936870b4bc6084fa4abc4
Chauncey <chaunceykiwi@gmail.com>
2015-12-29 00:08:01 -0800

    PotatoImage

---
+++ PotatoImage/PotatoImage/GalleryViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FirstViewController.swift
+//  PotatoImage
+//
+//  Created by Chauncey on 2015-12-28.
+//  Copyright © 2015 Chauncey. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PotatoImage/PotatoImage/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  PotatoImage
+//
+//  Created by Chauncey on 2015-12-28.
+//  Copyright © 2015 Chauncey. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
